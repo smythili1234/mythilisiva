863a6a3f09d6682e1620fe3fe1347232c9eabb48
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-01-21 11:12:42 +0530

    Create digit of integer.c

---
+++ digit of integer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,count=0;
+printf("enter the number");
+scanf("%d",&n);
+while(n!=0)
+{
+n=n/0
+count++
+}
+printf("no of digit of an integer is%d",count)
+return0
+}
