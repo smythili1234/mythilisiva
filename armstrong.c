d41eec87b558bf77bcd66ccf54c48a91a437e704
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-01-21 12:13:40 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int number,original number,remainder,result=0;
+printf("enter an n integer:");
+scanf("%d",&num);
+original number=number;
+while(original number!=0)
+[
+remainder=original number%10;
+result=remainder*remainder*original number/=10;
+}
+if(result==number)
+printf("%d is an armstrong number",number);
+else
+printf("%d is not an armstrong number",number);
+return0;
+}
