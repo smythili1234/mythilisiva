b77b361c66932368419e5c3da4f8ce36da2241ad
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-01-21 11:30:53 +0530

    Create palindrome or not.c

---
+++ palindrome or not.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+printf("enter the name mom is palindrome or not");
+else
+{
+printf(" enter it is palindrome")
+}
+printf("enter it is not palindrome");
+return0
+}
