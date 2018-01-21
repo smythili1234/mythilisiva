945153d79bc9ee50d1ef5cabc06c70d6f2b66217
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-01-21 09:55:35 +0530

    Create positive or negative.c

---
+++ positive or negative.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+{
+printf("%d",enter an integer);
+}
+else
+{
+preinf("enter a positive number");
+}
+else
+{
+printf("enter an negative number");
+}
+else
+return0
