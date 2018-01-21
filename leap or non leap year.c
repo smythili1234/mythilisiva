47ad1818db0d311755fe8bfa3bd3bc12d4cf1257
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-01-21 10:43:25 +0530

    Create leap or non leap year.c

---
+++ leap or non leap year.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h.
+int main()
+{
+if(year%4==0)
+{
+if(year%100==0)
+{
+if(year%400==0)
+printf("%d is leap year",year);
+else
+printf("%d is not a leap year",year);
+}
+else
+printf("%d is leap year",year);
+}
+else
+printf(%d is not a leap year",year);
+return0;
+}
