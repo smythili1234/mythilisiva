e45846eacf6511541440c8798ea60465f93d655d
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-01-21 10:15:15 +0530

    Create alphabet or not.c

---
+++ alphabet or not.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include,conio.h>
+void main()
+{
+char c;
+{
+printf("enter any alphabet");
+}
+scanf("%c",&n);
+{
+printf("enter the alphabet is yes");
+}
+{
+printf("enter the alphabet ism not");
+}
+return0
+}
