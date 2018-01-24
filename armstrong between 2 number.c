7948e2fcde523fe3b8c5c3acdb265a3754282633
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-01-24 16:11:30 +0530

    Create armstrong between 2 number.c

---
+++ armstrong between 2 number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main();
+{
+int low,high,i,temp1,temp2,remainder,n=0,result=0;
+printf("enter 2 number(integer):");
+scanf("%d,%d",&low&high);
+printf("armstrong number between %d are:",low,high);
+for(i=low+;i<hih,++i)
+{
+temp2=i;
+temp1=i;
+return0;
+}
+}
