88003a45fbb93aa738031ff62e6839b02287d776
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-07 10:26:04 +0000

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int arr[Min_SIZE]; 
+    int i, N;
+    printf("Enter size of array: ");
+    scanf("%d", &N);
+    printf("Enter %d elements in the array : ", N);
+    for(i=0; i<N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("\nElements in array are: ");
+    for(i=0; i<N; i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+
+    return 0;
+}
