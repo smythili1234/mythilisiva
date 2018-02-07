031f40e1eeea04f7c49e16a5e7c07a3b7408c738
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-07 11:09:27 +0000

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e,f;
+printf("enter the first value");
+scanf("%d%d",&a,&b);
+printf("enter the second value\n");
+scanf("%d",&d,&e);
+c=a-b;
+f=d-e;
+printf("%d=%d",c);
+printf("%d=%d",d);
+}
+return 0;
+}
