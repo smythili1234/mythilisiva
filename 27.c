45d64c47f4ece16270b635bd0ded7d1594a5bded
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-07 10:51:11 +0000

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter the number in words %c",ch);
+scanf("%c",&ch);
+if(ch>='1'&&ch<='9')
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
