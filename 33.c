51aefa3e05c6e0262c583d9099a65fb0eec1d9bb
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-09 10:30:48 +0000

    Create 33.c

---
+++ 33.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i;
+ch[20] a;
+a="hello world"
+for(i=0;i<=13;i++)
+{
+if(a==null)
+{
+printf("print number of space %d",a);
+}
+else
+{
+printf("present character %d",a);
+}
+}
+return 0;
+}
