35513b0274ebc175c083256df2a9b17a991917cf
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-09 10:19:36 +0000

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() 
+{
+	
+	char ch[100];
+	int i,k,count=1;
+	scanf("%[^\n]s",ch);
+	for(k=0;ch[k]!='\0';++k)
+	{
+		
+	}
+	for(i=0;i<k;i++)
+
+	{
+		count++;
+	}
+	printf("%d",count);
+	getch();
+}
