a82eeda4dbcd323c3bf8c6cf2f40007db11a78d3
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-09 11:10:16 +0000

    Create 40.c

---
+++ 40.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main() 
+{
+int i, n, a1 = 0, a2 = 1, n1;
+scanf("%d", &n);
+for (i = 1; i <= n; ++i)
+    {
+        printf("%d ", a1);
+        n1 = a1 + a2;
+        a1 = a2;
+        a2 = n1;
+    }
+getch();
+}
