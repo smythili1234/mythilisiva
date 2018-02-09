81723de2b853b1f08c35e6d1eed2fcb3bf103509
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-09 10:36:00 +0000

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	
+	char ch[100];
+	int i,k,count=1;
+	gets(ch);
+	for(k=0;ch[k]!='\0';++k)
+	{
+		
+	}
+	for(i=0;i<k;i++)
+	{
+	
+	if(ch[i]=='.'||ch[i]=='?'||ch[i]==!)
+	{
+		count++;
+	}
+		
+	}
+	printf("%d",count);
+	getch();
+}
