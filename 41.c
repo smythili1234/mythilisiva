62787d659e7da7802b7eae9464b45f8355ed5105
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-09 11:26:52 +0000

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int s,k,i;
+printf("enter string");
+scanf("%d",&s);
+printf("enter no of times the string is to be printed");
+scanf("%d",&k);
+while(k>0)
+{ 
+  for(i=0;i>k;i++)
+  {
+  printf("%d",s);
+  }
+ }
+ return 0;
+ }
