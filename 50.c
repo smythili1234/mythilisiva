7facc1bde639f49c638afeb8a07dde8fec8df403
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-19 16:31:36 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	if(a%==2)
+	{
+		printf("power");
+	}
+	else
+	{
+	printf("not a power");
+	}
+	return 0;
+}
