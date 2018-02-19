f21a40a9cbb328ec25227ac53608bd84f4470267
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-19 16:22:45 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the 1st integer a");
+	scanf("%d",&a);
+	printf("enter the 2nd number b");
+	scanf("%d",&b);
+	c=a+b;
+	printf("%d,c");
+	return 0;
+}
+
