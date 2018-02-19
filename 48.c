b1428fca9c2fe0dd190ef39c47b3c1535cbbac22
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-19 16:09:29 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,num[15],avg
+printf("enter the total number of numbers to be averaged");
+scanf("%d",&n);
+printf("enter the numbers");
+ for(i=0;i<n;i++)
+ {
+ scanf("%d",&num[15]);
+ }
+ printf("the entered numbers are %d",num);
+ avg=num[i]/n;
+ printf("the avg is %d",avg);
+ }
+ 
