f1ee3aae2faa5fb9e7b3a14643e419c873016acd
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-21 11:06:50 +0000

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,19 @@
+    #include <stdio.h>
+     
+    int main()
+    {
+    	int a,b,c;
+    	printf("enter the number");
+    	scanf("%d",&c);
+    	c=a*b;
+    	if(c%2==0)
+    	{
+    		printf("even");
+    	}
+    	else
+    	{
+    		printf("odd");
+    	}
+    	return 0;
+    }
+     
