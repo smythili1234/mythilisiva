237b780b12d6426f7929122d5da25d5e57ee7c4c
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-26 11:14:16 +0000

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+    void main()
+    {
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    a[0]=1;
+    a[1]=1;
+    for(i=2;i<n;i++)
+    {
+      a[i]=a[i-2]+a[i-1] ;
+     
+    }
+    for(i=0;i<n;i++)
+    {
+    printf("%d ",a[i]);
+    }
+    getch();
+    }
