28ff155aac905e663a04730bd62f0dc24bbc5685
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-02-28 10:24:06 +0000

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,15 @@
+    #include <stdio.h>
+    #include<string.h>
+    int main()
+    {
+    	int i,n;
+    	char a[20];
+    	scanf("%s",&a);
+    	scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+    	printf("%s",a);
+    }	
+     
+    return 0;
+    }
