c32a645570c33fbf824cf4e2bf8dece2393fee82
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-03-02 16:18:04 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int n,m,c;
+	printf("enter the number");
+	scanf("%d",&n);
+	printf("enter the number");
+	scanf("%d",&m);
+	c=n+m;
+	if(c%2==0)
+	{
+		printf("even");
+	}
+	else
+	{
+		printf("odd");
+	}
+	{
+	printf("%d,",c);
+}
+return 0;
+}
