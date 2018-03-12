7e3463bb3cd1ea60b3d54be11a477d108398f93a
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-03-12 10:44:40 +0000

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("yes");
+}
+else
+{
+    printf("no");
+}
+return 0;
+}
