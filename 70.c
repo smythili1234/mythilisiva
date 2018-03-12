e54c8771fd6d88ae4448a24ac27e23e9c0246e74
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-03-12 11:12:15 +0000

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int p,q;
+    printf("enter the number");
+    scanf("%d",&p);
+    q=p*2;
+    {
+    printf("nearest greater power of 2 %d",q);
+    }
+    return 0;
+}
