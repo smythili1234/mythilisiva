54e886bf1e4f43d6f06b215e3001ff31aa2b4c18
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-03-14 15:57:58 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+	char n;
+	printf("enter the character");
+	scanf("%s",&n);
+	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+{
+	printf("yes",n);
+}
+else
+{
+	printf("no",n);
+}
+return 0;
+}
