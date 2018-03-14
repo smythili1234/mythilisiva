8cd542ca4dfc0df5ce1bdc6c6857c3ab1957033c
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-03-14 16:20:57 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+   int l,r,n;
+   scanf("%d",&l);
+   scanf("%d",&r);
+   if((n>=l)&&(n<=r))
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+}
+scanf("%d",&n);
+return 0;
+}
