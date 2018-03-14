733d99aee2765e1e56da84c34fa240a4ac13a6ea
smythili1234 <31503962+smythili1234@users.noreply.github.com>
2018-03-14 16:28:51 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    if(a%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
